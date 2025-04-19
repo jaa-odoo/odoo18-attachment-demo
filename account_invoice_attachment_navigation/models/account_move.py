558656c4ae4713262b9b8fbc6bfe22d7e01c9ce7
Jainik Patel <pateljainikkumar@gmail.com>
2025-04-19 19:30:43 +0530

    [IMP] Sample data PDF and Video Added.

--- account_invoice_attachment_navigation/models/account_move.py
+++ account_invoice_attachment_navigation/models/account_move.py
@@ -6,13 +6,8 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    x_attachment_id = fields.Many2one(
-        comodel_name='ir.attachment',
-        string='Attachment',
-        readonly=False,
-        help="Attach a file to this invoice line for reference or documentation."
-    )
     attachments_ids = fields.Many2many(
         comodel_name='ir.attachment',
         string="Attachments",
+        help="Attach a file to this invoice line for reference or documentation."
     )
